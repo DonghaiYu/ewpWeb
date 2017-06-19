b01b335ee25e2c98b4902e9dbd284a7455a00a12
Dylan-laptop <815980445@qq.com>
2017-06-19 08:56:49 +0800

    fix

--- src/main/java/TestController.java
+++ src/main/java/TestController.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableAutoConfiguration
@@ -14,12 +14,22 @@ public class TestController {
 
     protected static Logger logger = LoggerFactory.getLogger(TestController.class);
 
-    @RequestMapping("/hello")
-    String home() {
+    @RequestMapping(value = "/hello")
+    String hello() {
         System.out.println("hello");
-        logger.debug("hello world");
         logger.info("hello test");
+        logger.error("no error");
         return "Hello world";
+    }
+
+    //返回jsp页面
+    @RequestMapping(value = "/")
+    String home() {
+        return "index";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestController.class, args);
     }
 
 }
